98c97710f4f5ae88ab141751a5408ea12f8abbe5
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2023-03-02 00:21:38 -0930

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,12 +9,13 @@
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int right = 0, left = 0, total = 0;
+
 	if (tree)
 	{
 		left = ((int)binary_tree_height(tree->left));
 		right = ((int)binary_tree_height(tree->right));
 		total = left - right;
-		return(total);
+		return (total);
 	}
 
 	return (0);
